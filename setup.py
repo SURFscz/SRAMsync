3b9e9fdad05ef825188746ff2315c0204ee17cc5
Gerben Venekamp <venekamp@gmail.com>
2021-11-18 11:16:40 +0100

    Adept setup.py for building proper modules

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name="sync-with-sram",
-    version="1.0",
-    py_module=["sync_with_sram"],
-    install_requires=["Click", "future-fstrings"],
-    entry_points={"console_scripts": ["sync-with-sram = sync_with_sram:cli"]},
+    name="SRAMsync",
+    version="1.0.0",
+    author="Gerben Venekamp",
+    author_email="gerben.venekamp@surf.nl",
+    description="Reading entries and attributes from SRAM LDAP and process them",
+    url="https://github.com/venekamp/CUA-sync",
+    install_requires=["Click", "pyldap", "jsonschema", "pyyaml"],
+    entry_points={"console_scripts": ["sync-with-sram = SRAMsync.sync_with_sram:cli"]},
+    python_requires=">=3.6",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPLv3",
+        "Operating System :: OS Independent",
+    ],
 )
