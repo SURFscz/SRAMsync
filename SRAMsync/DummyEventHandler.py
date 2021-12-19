413f9b1770a2076704bb3a1049ea30601cba2dd2
Gerben Venekamp <venekamp@gmail.com>
2021-12-19 09:38:09 +0100

    Remove generator dependency and add debug logging

--- SRAMsync/DummyEventHandler.py
+++ SRAMsync/DummyEventHandler.py
@@ -1,36 +1,49 @@
+from SRAMsync.SRAMlogger import logger
 from SRAMsync.EventHandler import EventHandler
 
 
 class DummyEventHandler(EventHandler):
-    def __init__(self, generator):
-        self.generator = generator
+    def __init__(self, cfg):
+        logger.debug("DummyEventHandler.__init__(cfg)")
+        logger.debug(cfg)
+        pass
 
     def add_new_user(self, group, givenname, sn, user, mail):
-        return super().add_new_user(group, givenname, sn, user, mail)
+        logger.debug(f"  add_new_user({group}, {givenname}, {sn}, {user}, {mail}")
+        return
 
     def start_of_service_processing(self, co):
-        return super().start_of_service_processing(co)
+        logger.debug(f"  start_of_service_processing({co})")
+        return
 
     def add_public_ssh_key(self, user, key):
-        return super().add_public_ssh_key(user, key)
+        logger.debug(f"    add_public_ssh_key({user}, {key})")
+        return
 
     def delete_public_ssh_key(self, user, key):
-        return super().delete_public_ssh_key(user, key)
+        logger.debug(f"    delete_public_ssh_key({self}, {user}, {key})")
+        return
 
     def add_new_group(self, group, attributes):
-        return super().add_new_group(group, attributes)
+        logger.debug(f"  add_new_group({group}, {attributes})")
+        return
 
     def remove_group(self, group, attributes):
-        return super().remove_group(group, attributes)
+        logger.debug(f"  remove_group({group}, {attributes})")
+        return
 
     def add_user_to_group(self, group, user, attributes: list):
-        return super().add_user_to_group(group, user, attributes)
+        logger.debug(f"  add_user_to_group({group}, {user}, {attributes})")
+        return
 
     def remove_user_from_group(self, group, user, attributes: list):
-        return super().remove_user_from_group(group, user, attributes)
+        logger.debug(f"  remove_user_from_group({group}, {user}, {attributes})")
+        return
 
     def remove_graced_user_from_group(self, group, user, attributes):
-        return super().remove_graced_user_from_group(group, user, attributes)
+        logger.debug(f"  remove_graced_user_from_group({group}, {user}, {attributes})")
+        return
 
     def finialize(self):
-        return super().finialize()
+        logger.debug(f"  finialize()")
+        return
