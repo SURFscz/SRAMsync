28fa087a94203de99ffab90d17a826783c00d2a4
Gerben Venekamp <venekamp@gmail.com>
2021-12-23 07:06:40 +0100

    Remove leftover 'generator' code

--- SRAMsync/EventHandler.py
+++ SRAMsync/EventHandler.py
@@ -2,45 +2,45 @@ from abc import ABC, abstractmethod
 
 
 class EventHandler(ABC):
-    def __init__(self, generator):
-        self.generator = generator
+    def __init__(self):
+        pass
 
     @abstractmethod
     def start_of_service_processing(self, co):
-        self.generator.start_of_service_processing(co)
+        pass
 
     @abstractmethod
     def add_new_user(self, group, givenname, sn, user, mail):
-        self.generator.add_new_user(group, givenname, sn, user, mail)
+        pass
 
     @abstractmethod
     def add_public_ssh_key(self, user, key):
-        self.generator.add_public_ssh_key(user, key)
+        pass
 
     @abstractmethod
     def delete_public_ssh_key(self, user, key):
-        self.generator.delete_public_ssh_key(user, key)
+        pass
 
     @abstractmethod
     def add_new_group(self, group, attributes):
-        self.generator.add_new_group(group, attributes)
+        pass
 
     @abstractmethod
     def remove_group(self, group, attributes):
-        self.generator.remove_group(group, attributes)
+        pass
 
     @abstractmethod
     def add_user_to_group(self, group, user, attributes):
-        self.generator.add_user_to_group(group, user, attributes)
+        pass
 
     @abstractmethod
     def remove_user_from_group(self, group, user, attributes: list):
-        self.generator.remove_user_from_group(group, user)
+        pass
 
     @abstractmethod
     def remove_graced_user_from_group(self, group, user, attributes):
-        self.generator.remove_graced_user_from_group(group, user, attributes)
+        pass
 
     @abstractmethod
     def finalize(self):
-        self.generator.finalize()
+        pass
