f91c02f42b7c5024350f4069da7580aa6a15298a
Gerben Venekamp <venekamp@gmail.com>
2022-02-24 13:48:34 +0100

    Finish class implementation

--- SRAMsync/cba_script_generator.py
+++ SRAMsync/cba_script_generator.py
@@ -1,33 +1,64 @@
-from distutils.command.config import config
-import importlib
-from datetime import datetime
-import json
-from re import I
-from jsonschema import validate, ValidationError
+"""
+Additional functionality for CBA related accounting. The CbaScriptGenerator
+class is derived from CuaScriptGenerator, because of its strong relation. By
+doing so, the functionallity of CuaScriptGenerator can be resused here.
+"""
 
+from jsonschema import ValidationError, validate
+
+from .cua_script_generator import CuaScriptGenerator
 from .sync_with_sram import ConfigValidationError
-from .common import render_templated_string
-from .sramlogger import logger
-from .EventHandler import EventHandler
-from .CuaScriptGenerator import CuaScriptGenerator
 
 
 class CbaScriptGenerator(CuaScriptGenerator):
+    """
+    Class for inserting additional commands in the bash script that the
+    CuaScriptGenerator class generates. These extra command insert relevant
+    account information.
+    """
+
     _schema = {
         "$schema": "http://json-schema.org/draft/2020-12/schema",
         "type": "object",
         "properties": {
-            "cba_account": {"type": "string"},
+            "cba_add_cmd": {"type": "string"},
+            "cba_del_cmd": {"type": "string"},
+            "cba_machine": {"type": "string"},
+            "cba_budget_account": {"type": "string"},
             "cua_config": {"type": "object"},
         },
-        "required": ["cba_account", "cua_config"],
+        "required": ["cba_add_cmd", "cba_del_cmd", "cba_machine", "cba_budget_account", "cua_config"],
     }
 
-    def __init__(self, service, cfg, path):
+    def __init__(self, service: str, cfg: dict, path: str) -> None:
         try:
             validate(schema=CbaScriptGenerator._schema, instance=cfg)
             super().__init__(service, cfg["cua_config"], path)
+            self.cfg = cfg
         except ConfigValidationError as e:
             raise e
         except ValidationError as e:
             raise ConfigValidationError(e, path) from e
+
+    def add_new_user(self, group: str, givenname: str, sn: str, user: str, mail: str) -> None:
+        """add_new_user event."""
+        super().add_new_user(group, givenname, sn, user, mail)
+        self.print("# Adding user CBA account.")
+        self.insert_cba_command(self.cfg["cba_add_cmd"], user)
+
+    def remove_user_from_group(self, group: str, user: str, attributes: list):
+        """remove_user_from_group event."""
+        super().remove_user_from_group(group, user, attributes)
+        self.insert_cba_command(self.cfg["cba_del_cmd"], user)
+
+    def remove_graced_user_from_group(self, group: str, user: str, attributes: list) -> None:
+        """remove_graced_user_from_group event."""
+        super().remove_graced_user_from_group(group, user, attributes)
+        self.insert_cba_command(self.cfg["cba_del_cmd"], user)
+
+    def insert_cba_command(self, cmd: str, user: str) -> None:
+        """Insert the cba command with arguments into the generated bash script."""
+        self.print(
+            f"{cmd} --machine {self.cfg['cba_machine']} "
+            f"--account {self.cfg['cba_budget_account']} --user {user}\n"
+        )
