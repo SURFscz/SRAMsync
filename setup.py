d18f1ffda694f35c3ce130ac123681e41d95ed35
Gerben Venekamp <venekamp@gmail.com>
2024-07-19 10:01:15 +0200

    Update version

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ from setuptools import find_packages, setup
 
 setup(
     name="SRAMsync",
-    version="4.3.2",
+    version="4.4.0",
     author="Gerben Venekamp",
     author_email="gerben.venekamp@surf.nl",
     description="Reading entries and attributes from SRAM LDAP and process them",
     url="https://github.com/SURFscz/SRAMsync/",
     install_requires=["Click", "pyldap", "jsonschema", "pyyaml", "click_logging"],
     entry_points={"console_scripts": ["sync-with-sram = SRAMsync.sync_with_sram:cli"]},
-    python_requires=">=3.7",
+    python_requires=">=3.9",
     packages=find_packages(),
     include_package_data=True,
     classifiers=[
